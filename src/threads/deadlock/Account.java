cf2eff7ca47c389218fd6d9b692c2ff7ccf3fc1e
raman garg <raman.g.garg@oracle.com>
2017-03-14 22:21:11 +0530

    deadlock related code

---
+++ src/threads/deadlock/Account.java
@@ -0,0 +1,26 @@
+package threads.deadlock;
+
+/**
+ * Created by ramgarg on 14-Mar-17.
+ */
+public class Account {
+
+    private int balance = 10000;
+    public void deposit(int amount){
+        balance += amount;
+    }
+
+    public void withdraw(int amount){
+        balance -= amount;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public static void transfer(Account acc1, Account acc2, int amount){
+        acc1.withdraw(amount);
+        acc2.deposit(amount);
+    }
+
+}
---
+++ src/threads/deadlock/App.java
@@ -0,0 +1,43 @@
+package threads.deadlock;
+
+/**
+ * Created by ramgarg on 13-Mar-17.
+ */
+public class App {
+    public static void main(String[] args){
+        final Runner proc = new Runner();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    proc.firstThread();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    proc.secondThread();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        proc.finished();
+    }
+}
